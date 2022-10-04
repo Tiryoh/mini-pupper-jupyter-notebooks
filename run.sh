2d24b9d0dad119b0ea2054073978de65c4474b41
Daisuke Sato <tiryoh@gmail.com>
2022-10-05 04:38:57 +0900

    Update Dockerfile

--- run.sh
+++ run.sh
@@ -1,19 +1,18 @@
 #!/bin/bash -eu
 
-#DOCKER=tiryoh/pytorch:1.12.1
-#DOCKER=tiryoh/jupyter-ros:noetic
 DOCKER=tiryoh/conda-jupyter-ros:noetic
 
 set -x
 
 #       --privileged \
-
-docker run --rm -it -u $(id -u):$(id -g) \
-       --device /dev/video0 \
-       --group-add video \
-       -v /dev/bus/usb:/dev/bus/usb --device-cgroup-rule='c 189:* rmw' \
-       -p 8888:8888 -v $(pwd)/notebook:/workspace \
-       --network=host \
-       ${DOCKER}
+#       --device /dev/video0 \
+#       --group-add video \
+#       -v /dev/bus/usb:/dev/bus/usb --device-cgroup-rule='c 189:* rmw' \
+#       -p 8888:8888 \
 #       ${DOCKER} \
 #       jupyter lab --notebook-dir=/workspace --ip 0.0.0.0 --no-browser
+
+docker run --rm -it -u $(id -u):$(id -g) \
+       -v $(pwd)/notebook:/workspace \
+       --network=host \
+       ${DOCKER}
